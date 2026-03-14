65af0dadcf59e4e270f921b63dce6dde609eff43
Sérgio Luiz Resende <sergio.resende@gmail.com>
2026-03-14 15:00:22 -0300

    inclusão de testes

--- src/test/java/com/legsoft/rabbitmq/RabbitmqApplicationTests.java
+++ src/test/java/com/legsoft/rabbitmq/RabbitmqApplicationTests.java
@@ -1,10 +1,15 @@
 package com.legsoft.rabbitmq;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class RabbitmqApplicationTests {
+
+	@MockitoBean
+	private ConnectionFactory connectionFactory;
 
 	@Test
 	void contextLoads() {
